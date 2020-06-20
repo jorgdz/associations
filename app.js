7f765dd6696b8f8b7eb06a3c23a8e72530ffa409
Jorge Diaz Montoya <57382609+jorgdz@users.noreply.github.com>
2020-06-20 12:20:35 -0500

    Image products (#4)
    * Add principal image to products
    * Show all categories in navbar
    * Fix procfile
    * Show product view

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
+var hbsHelper = require("./config/hbsHelper");
 
 var app = express();
 
@@ -15,20 +16,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
 hbs.registerPartials(__dirname + "/views/partials");
-
-hbs.registerHelper("trimText", function (str) {
-  return str.substring(0, 25);
-});
-
-hbs.registerHelper("priceInteger", function (price) {
-  var entPart = (price + "").split(".")[0];
-  return entPart;
-});
-
-hbs.registerHelper("priceDecimal", function (price) {
-  var decPart = (price + "").split(".")[1];
-  return `${decPart == undefined ? "" : "." + decPart}`;
-});
 
 app.use(logger("dev"));
 app.use(express.json());
--- src/Controllers/HomeController.js
+++ src/Controllers/HomeController.js
@@ -2,6 +2,15 @@
 
 const User = require("../Models").User;
 const Product = require("../Models").Product;
+const Image = require("../Models").Image;
+const Category = require("../Models").Category;
+
+const {
+  StylesHome,
+  StylesProduct,
+  JsHome,
+  JsProduct,
+} = require("../../config/static");
 
 exports.index = async (req, res, next) => {
   const products = await Product.findAll({
@@ -10,30 +19,60 @@ exports.index = async (req, res, next) => {
         model: User,
         as: "user",
       },
-    ],
-    order: [["id", "DESC"]],
-  });
-
-  res.render("index", { title: "Productos", products: products });
-};
-
-exports.show = (req, res, next) => {
-  return Product.findAll({
-    where: { id: req.params.id },
-    include: [
       {
-        model: User,
-        as: "user",
+        model: Image,
+        as: "images",
       },
     ],
-  })
-    .then((product) => {
-      res.render("show", {
-        title: `Producto ${product[0].name}`,
-        product: product[0],
-      });
-    })
-    .catch((err) => {
-      res.render("No se ha encontrado el producto");
+    order: [
+      ["id", "DESC"],
+      [{ model: Image, as: "images" }, "principal", "DESC"],
+    ],
+  });
+
+  const categories = await Category.findAll({
+    order: [["name", "ASC"]],
+  });
+
+  res.render("index", {
+    title: "Productos",
+    products: products,
+    categories: categories,
+    styles: StylesHome,
+    javascripts: JsHome,
+  });
+};
+
+exports.show = async (req, res, next) => {
+  try {
+    const product = await Product.findAll({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: User,
+          as: "user",
+        },
+        {
+          model: Image,
+          as: "images",
+        },
+      ],
+      order: [[{ model: Image, as: "images" }, "principal", "DESC"]],
     });
+
+    const categories = await Category.findAll({
+      order: [["name", "ASC"]],
+    });
+
+    res.render("show", {
+      title: `Producto ${product[0].name}`,
+      product: product[0],
+      categories: categories,
+      styles: StylesProduct,
+      javascripts: JsProduct,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("No se ha encontrado el producto");
+  }
 };
